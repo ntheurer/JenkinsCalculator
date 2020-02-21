e51037818d9519bb692d6d2a619d8ccbd10bdbd9
Nicole Gottfredson <nicole.gottfredson@gmail.com>
2020-02-21 13:35:12 -0700

    More changes

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -15,7 +15,6 @@ public class CalculatorTest {
         assertEquals(6 , myCalculator.add(0,6));
         assertEquals(6 , myCalculator.add(7,-1));
         assertEquals( -6, myCalculator.add(-3,-3));
-
     }
 
 
